d7c733797bc8140c3b774f35c20ad9f3a2c6158b
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-01-23 18:35:36 +0100

    feat: added user routes and created model and controllers folder

---
+++ Models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true, minlenght: 3, maxlenght: 30 },
+        email: { type: String, required: true, minlenght: 3, maxlenght: 200, unique: true },
+        password: { type: String, required: true, minlenght: 3, maxlenght: 10 },
+
+    }, {
+    timestamps: true,
+}
+);
+
+const userModel = mongoose.model('User', userSchema)
+
+module.exports = userModel; 
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose  = require('mongoose');
+const userRoute = require('./Routes/userRoute')
 
 const app = express();
 require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
+
+// CRUD
+app.get('/', (req, res)=>{
+    res.send('Welcome to our chat app APIs..')
+})
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
@@ -19,3 +25,4 @@ mongoose.connect(uri)
     .then(()=>console.log('MongoDB connection established'))
     .catch((error)=>console.log('MongoDB connection failed: ', error.message))
 
+ 
