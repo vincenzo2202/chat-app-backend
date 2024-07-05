341b11d88b2a4aae1f7dc5cfe0fb92776d28cee0
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-07-05 17:01:41 +0200

    UPDATE: update back code

--- Models/userModel.js
+++ Models/userModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true, minlenght: 3, maxlenght: 30 },
-        email: { type: String, required: true, minlenght: 3, maxlenght: 200, unique: true },
-        password: { type: String, required: true, minlenght: 3, maxlenght: 10 },
+        name: { type: String, required: true, minlength : 3, maxlength : 30 },
+        email: { type: String, required: true, minlength : 3, maxlength : 200, unique: true },
+        password: { type: String, required: true, minlength : 3, maxlength : 60 },
 
     }, {
     timestamps: true,
@@ -12,5 +12,5 @@ const userSchema = new mongoose.Schema(
 );
 
 const userModel = mongoose.model('User', userSchema)
-
+// P@ssw0rd123!
 module.exports = userModel; 
--- Routes/messageRoute.js
+++ Routes/messageRoute.js
@@ -4,8 +4,7 @@ const { createMessage, getMessages } = require('../Controllers/messageController
 const router = express.Router();
 
 router.post('/', createMessage);
-router.post('/:chatId', getMessages);
- 
+router.get('/:chatId', getMessages);
 
 module.exports = router;
 
